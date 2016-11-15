e190e56f70ea58c94876f668bb8d9403c187dd1e
Ed Ball <ed.ball@logos.com>
2016-11-14 19:49:40 -0800

    Push broken test to confirm build failure.

--- example/ts/test/exampleApiTests.ts
+++ example/ts/test/exampleApiTests.ts
@@ -20,17 +20,55 @@ function createFetchResponse(status: number, body?: any) {
 
 describe('createHttpClient', () => {
 
-	it('get widgets returns 200', () => {
+	it('get widget with ID', () => {
 		return createHttpClient({
 			fetch: (uri, request) => {
-				uri.should.equal('http://local.example.com/v1/widgets');
+				uri.should.equal('http://local.example.com/v1/widgets/xyzzy');
 				request.method.should.equal('GET');
 				expect(request.headers).to.not.exist;
 				expect(request.body).to.not.exist;
-				return createFetchResponse(200, {});
+				return createFetchResponse(200, {
+					id: 'xyzzy',
+					name: 'Xyzzy'
+				});
 			}
-		}).getWidgets({}).then(result => {
-			result.value.should.deep.equal({});
+		}).getWidget({
+			id: 'xyzzy'
+		}).then(result => {
+			result.value.should.deep.equal({
+				widget: {
+					id: 'xyzzy',
+					name: 'Xyzzy'
+				}
+			});
+		});
+	});
+
+	it('get widget with missing ID', () => {
+		return createHttpClient({
+			fetch: (uri, request) => {
+				throw new Error();
+			}
+		}).getWidget({}).then(result => {
+			result.error.should.deep.equal({
+				code: 'invalidRequest',
+				message: 'The request field \'id\' is required.'
+			})
+		});
+	});
+
+	it('get widget with blank ID', () => {
+		return createHttpClient({
+			fetch: (uri, request) => {
+				throw new Error();
+			}
+		}).getWidget({
+			id: ''
+		}).then(result => {
+			result.error.should.deep.equal({
+				code: 'invalidRequest',
+				message: 'The request field \'id\' is required.'
+			})
 		});
 	});
 
