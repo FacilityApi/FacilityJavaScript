f50def3cf5282c60b3fcdcd85ce184cb9d6631c5
Dave Dunkin <dave.dunkin@logos.com>
2018-01-25 14:26:49 -0700

    Enable strict null checks for example/ts.

--- example/ts/test/exampleApiTests.ts
+++ example/ts/test/exampleApiTests.ts
@@ -11,7 +11,7 @@ function createFetchResponse(status: number, body?: any) {
 			} else if (typeof body === 'string' && name.toLowerCase() === 'content-type') {
 				return 'text/html';
 			}
-			return undefined;
+			return null;
 		}
 	};
 	const json = () => Promise.resolve(body);
@@ -24,7 +24,7 @@ describe('createHttpClient', () => {
 		return createHttpClient({
 			fetch: (uri, request) => {
 				uri.should.equal('http://local.example.com/v1/widgets/xyzzy');
-				request.method.should.equal('GET');
+				expect(request.method).to.equal('GET');
 				expect(request.headers).to.not.exist;
 				expect(request.body).to.not.exist;
 				return createFetchResponse(200, {
@@ -35,7 +35,7 @@ describe('createHttpClient', () => {
 		}).getWidget({
 			id: 'xyzzy'
 		}).then(result => {
-			result.value.should.deep.equal({
+			expect(result.value).to.deep.equal({
 				widget: {
 					id: 'xyzzy',
 					name: 'Xyzzy'
@@ -50,7 +50,7 @@ describe('createHttpClient', () => {
 				throw new Error();
 			}
 		}).getWidget({}).then(result => {
-			result.error.should.deep.equal({
+			expect(result.error).to.deep.equal({
 				code: 'invalidRequest',
 				message: 'The request field \'id\' is required.'
 			})
@@ -65,7 +65,7 @@ describe('createHttpClient', () => {
 		}).getWidget({
 			id: ''
 		}).then(result => {
-			result.error.should.deep.equal({
+			expect(result.error).to.deep.equal({
 				code: 'invalidRequest',
 				message: 'The request field \'id\' is required.'
 			})
