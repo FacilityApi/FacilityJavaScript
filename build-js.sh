43f475bf14688558548779d381133db100bbe79c
Dave Dunkin <dave.dunkin@logos.com>
2018-03-30 13:06:18 -0700

    Echo npm-1.2.3 on build.

--- build-js.sh
+++ build-js.sh
@@ -15,3 +15,8 @@ pushd example/ts >/dev/null
 npm install
 npm run test
 popd >/dev/null
+
+echo TRAVIS_TAG=$TRAVIS_TAG
+if [[ $TRAVIS_TAG =~ ^npm-v[0-9]+\.[0-9]+\.[0-9]+(-.*)?$ ]] ; then
+    echo $TRAVIS_TAG is deployable
+fi
