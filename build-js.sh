4c5f25d34e730413aef6993cd75cc71bad46173b
Ed Ball <ed.ball@faithlife.com>
2020-06-10 21:33:42 -0700

    Publish on master build.

--- build-js.sh
+++ build-js.sh
@@ -15,3 +15,11 @@ pushd example/ts >/dev/null
 npm install
 npm run test
 popd >/dev/null
+
+pushd ts >/dev/null
+if [[ "${APPVEYOR_REPO_BRANCH}" == "master" ]] && [[ -z "${APPVEYOR_PULL_REQUEST_NUMBER}" ]]
+then
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> .npmrc
+npm publish || true
+fi
+popd >/dev/null
--- publish-js.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-cd ts
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> .npmrc
-npm publish
