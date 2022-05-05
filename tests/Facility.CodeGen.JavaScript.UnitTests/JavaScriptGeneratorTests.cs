a98fea43bceb25ed66be00efb6b50f0eee2c5f65
Joseph Stewart <joseph.stewart@faithlife.com>
2022-05-05 12:37:23 -0700

    Allow empty response bodies in generated typescript client for boolean body fields.

--- tests/Facility.CodeGen.JavaScript.UnitTests/JavaScriptGeneratorTests.cs
+++ tests/Facility.CodeGen.JavaScript.UnitTests/JavaScriptGeneratorTests.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using Facility.Definition;
+using Facility.Definition.CodeGen;
 using Facility.Definition.Fsd;
 using NUnit.Framework;
 
@@ -31,24 +32,29 @@ namespace Facility.CodeGen.JavaScript.UnitTests
 		}
 
 		[Test]
+		public void GenerateExampleApiTypeScript_DoesntExpectResponseBodyForBooleanFields()
+		{
+			var result = GenExampleApi();
+			var codeFile = result.Files.Single(f => f.Name == "exampleApi.ts");
+
+			const string expectedCheckWithoutBody = @"
+				else if (status === 304) {
+					value = { notModified: true };
+				}";
+
+			const string expectedCheckWithBody = @"
+				else if (status === 202 && result.json) {
+					value = { job: result.json } as IEditWidgetResponse;
+				}";
+
+			Assert.That(codeFile.Text, Contains.Substring(expectedCheckWithoutBody));
+			Assert.That(codeFile.Text, Contains.Substring(expectedCheckWithBody));
+		}
+
+		[Test]
 		public void GenerateExampleApiTypeScript_IncludesEnums()
 		{
-			ServiceInfo service;
-			const string fileName = "Facility.CodeGen.JavaScript.UnitTests.ExampleApi.fsd";
-			var parser = new FsdParser();
-			var stream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream(fileName)!;
-			Assert.IsNotNull(stream);
-			using (var reader = new StreamReader(stream))
-				service = parser.ParseDefinition(new ServiceDefinitionText(Path.GetFileName(fileName), reader.ReadToEnd()));
-
-			var generator = new JavaScriptGenerator
-			{
-				GeneratorName = "JavaScriptGeneratorTests",
-				TypeScript = true,
-				NewLine = "\n",
-			};
-			var result = generator.GenerateOutput(service);
-			Assert.IsNotNull(result);
+			var result = GenExampleApi();
 
 			var typesFile = result.Files.Single(f => f.Name == "exampleApiTypes.ts");
 			const string expectedEnums = @"
@@ -79,6 +85,25 @@ export enum ObsoleteEnum {
 			const string expectedEnumUsage = @"widgetField?: WidgetField;";
 
 			Assert.That(typesFile.Text, Contains.Substring(expectedEnumUsage));
+		}
+
+		private CodeGenOutput GenExampleApi()
+		{
+			ServiceInfo service;
+			const string fileName = "Facility.CodeGen.JavaScript.UnitTests.ExampleApi.fsd";
+			var parser = new FsdParser();
+			var stream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream(fileName)!;
+			Assert.IsNotNull(stream);
+			using (var reader = new StreamReader(stream))
+				service = parser.ParseDefinition(new ServiceDefinitionText(Path.GetFileName(fileName), reader.ReadToEnd()));
+
+			var generator = new JavaScriptGenerator
+			{
+				GeneratorName = "JavaScriptGeneratorTests",
+				TypeScript = true,
+				NewLine = "\n",
+			};
+			return generator.GenerateOutput(service);
 		}
 	}
 }
