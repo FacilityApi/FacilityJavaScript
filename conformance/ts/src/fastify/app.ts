b4f1516a3c1fcf1da33a4756999d2556b178f077
Mitch Rosenburg <mitchknife@gmail.com>
2023-12-15 13:54:24 -0800

    Add "hand rolled" ConformanceApi plugin.
    Codegen is incoming!

--- conformance/ts/src/fastify/app.ts
+++ conformance/ts/src/fastify/app.ts
@@ -1,12 +1,15 @@
+import fs from "fs";
+import path from "path";
 import { FastifyPluginAsync } from "fastify";
+import { ConformanceApiService } from "../conformanceApiService.js";
+import { handRolledPlugin } from "./handRolledPlugin.js";
+import conformanceTestsJson from '../../../ConformanceTests.json';
 
 export type AppOptions = {};
 const options: AppOptions = {};
 
 const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
-	fastify.get('/', () => {
-		return { hello: 'world' };
-	});
+	fastify.register(handRolledPlugin, { api: new ConformanceApiService(conformanceTestsJson.tests) });
 };
 
 export default app;
